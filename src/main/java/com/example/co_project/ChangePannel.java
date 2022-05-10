b8ab41e1e5ae377d6fc743683ee3d70ab2b0c5f0
Rares Sabau <raressabau1284@gmail.com>
2022-05-11 00:16:24 +0300

    Changed hello panel and created the rest of the panels

---
+++ src/main/java/com/example/co_project/ChangePannel.java
@@ -0,0 +1,66 @@
+package com.example.co_project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class ChangePannel {
+
+    public ChangePannel(){
+
+    }
+
+    @FXML
+    private Button welcome_button;
+    @FXML
+    private Button digits;
+    @FXML
+    private Button backDigits;
+    @FXML
+    private Button floating;
+    @FXML
+    private Button caching;
+
+    public void changePannel(ActionEvent event) throws IOException{
+        Pannel();
+    }
+
+    private void Pannel() throws IOException {
+        Main m = new Main();
+
+        m.changeScene("afterHello.fxml");
+    }
+
+    public void changeDigits(ActionEvent event) throws IOException{
+        Digits();
+    }
+
+    private void Digits() throws IOException {
+        Main m = new Main();
+
+        m.changeScene("afterDigits.fxml");
+    }
+
+    public void changeFloating(ActionEvent event) throws IOException{
+        Floating();
+    }
+
+    private void Floating() throws IOException {
+        Main m = new Main();
+
+        m.changeScene("afterFloating.fxml");
+    }
+
+    public void changeCaching(ActionEvent event) throws IOException{
+        Caching();
+    }
+
+    private void Caching() throws IOException {
+        Main m = new Main();
+
+        m.changeScene("afterCaching.fxml");
+    }
+
+}
